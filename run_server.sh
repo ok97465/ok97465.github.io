ec4392c03caf15a2278fbac25dff5b6c3eaae966
ok97465 <ok97465@gmail.com>
2020-02-02 21:36:15 +0900

    update path

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/home/ok97465/anaconda3/bin/python /home/ok97465/python/BlogSrcByJupyter/convert_jupyter_to_md.py
+/home/ok97465/anaconda3/bin/python /home/ok97465/codepy/BlogSrcByJupyter/convert_jupyter_to_md.py
 
 jekyll serve
